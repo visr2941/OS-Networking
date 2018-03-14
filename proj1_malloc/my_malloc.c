0075c12c862577a431c5395790d12d1df41f4dcd
visr2941 <visr2941@colorado.edu>
2018-03-14 01:05:23 -0500

    modified my_alloc func

--- proj1_malloc/my_malloc.c
+++ proj1_malloc/my_malloc.c
@@ -7,15 +7,18 @@
 /*********************************************************************************
  ***************** MACRO Definitions *********************************************
  ********************************************************************************/
-#define SIZE_OVERHD sizeof(malloc_overhead_t)
+
+#define SIZE_OVERHD     (sizeof(malloc_overhead_t))
+#define ALIGN8(size)     if(size%8) size = size - (size%8 - 8) 
 
 
 /*********************************************************************************
  ***************** Type Definitions **********************************************
  ********************************************************************************/
+
 typedef long unsigned int ptr_to_int_t;
 
-/* double link list structure for the allocation overhead */
+// double link list structure for the allocation overhead
 typedef struct malloc_overhead 
 {
     struct malloc_overhead * next;
@@ -29,9 +32,10 @@ typedef struct malloc_overhead
 /*********************************************************************************
  ***************** GLOBAL Variables **********************************************
  ********************************************************************************/
-/* defining a global variable to track the head pointer of the 
-   free memory list */
+
+// defining a global variable to track the head pointer of the free memory list
 malloc_overhead_t * hd_free_list = NULL;
+// defining an array for malloc allocation
 char arr_malloc[MAX_MALLOC_SIZE];
 
 
@@ -43,7 +47,7 @@ void InitMyMalloc( void )
 {
     hd_free_list = (malloc_overhead_t *) arr_malloc;
     
-    //initializing the free memory list
+    /* initializing the free memory list */
     hd_free_list->next = NULL;
     hd_free_list->prev = NULL;
     hd_free_list->size = MAX_MALLOC_SIZE - SIZE_OVERHD;
@@ -51,16 +55,23 @@ void InitMyMalloc( void )
     
 }
 
+/************************************************************************* 
+*  Description: 
+*            This function provides pointer to a valid memory of requested
+*            size based on the FIRST FIT method. 
+*  Parameter:  
+*            int size - size of memory required in bytes 
+*  return:
+*            void * - a pointer of memory location as a void *
+**************************************************************************/ 
 
 void* MyMalloc(int size)
 {
-    malloc_overhead_t * ptr_to_return = hd_free_list;
-    int temp = size%8;
+    malloc_overhead_t * ptr_to_return          = hd_free_list;
+    malloc_overhead_t * ptrToFreeMemAfterSplit = NULL;
     
-    
-    // for making allocation 8-bit aligned
-    if(temp)
-        size = size - temp + 8;
+    // Make the size divisible by 8 (greater or equal to size)
+    ALIGN8(size);
     
     // finding the first fit
     while(size > ptr_to_return->size)
@@ -69,39 +80,50 @@ void* MyMalloc(int size)
         if(!ptr_to_return)
             return NULL;
     };
-    
-    // if we can't put overhead in remaining free space,
-    // we will allocate that memory to the user (wasted)
+
+    /* if we can't put overhead in remaining free space,
+     * we will allocate that memory to the user (wasted)
+     * otherwise split the free block into two memory
+     * blocks, first one to be allocated and next as free.
+     */
     if(ptr_to_return->size - size <= SIZE_OVERHD)
-        size = ptr_to_return->size;
-    
-    
-    // remove the block from the free memory list   
-    if(ptr_to_return == hd_free_list)
     {
-        if(!ptr_to_return->next)
+        size = ptr_to_return->size;
+        if(ptr_to_return->next)
+            ptrToFreeMemAfterSplit = ptr_to_return->next;
+        else
         {
-            hd_free_list = (malloc_overhead_t * )((char *)ptr_to_return + SIZE_OVERHD + size);
-            hd_free_list->size = ptr_to_return->size - size - SIZE_OVERHD;
-            hd_free_list->buffer = (unsigned char *)((char *)hd_free_list + SIZE_OVERHD);
+            ptrToFreeMemAfterSplit         = (malloc_overhead_t *) (arr_malloc+MAX_MALLOC_SIZE);
+            ptrToFreeMemAfterSplit->size   = 0;
+            ptrToFreeMemAfterSplit->buffer = NULL;
+            ptrToFreeMemAfterSplit->prev   = NULL;
+            ptrToFreeMemAfterSplit->next   = NULL;
         }
-        else 
-        {
-            hd_free_list = ptr_to_return->next;
-            hd_free_list->size = ptr_to_return->next->size;
-            hd_free_list->buffer = ptr_to_return->next->buffer;
-        }
-            
     }
     else
     {
-        (ptr_to_return->prev)->next = ptr_to_return->next;
-        if(ptr_to_return->next)
-            ptr_to_return->next->prev = ptr_to_return->prev;
+        ptrToFreeMemAfterSplit         =  (malloc_overhead_t *) ((char *) ptr_to_return + SIZE_OVERHD + size);
+        ptrToFreeMemAfterSplit->size   =  ptr_to_return->size - size - SIZE_OVERHD;
+        ptrToFreeMemAfterSplit->buffer =  (unsigned char *) ((char *) ptrToFreeMemAfterSplit + SIZE_OVERHD);
+        ptrToFreeMemAfterSplit->next   =  ptr_to_return->next;
+        ptrToFreeMemAfterSplit->prev   =  ptr_to_return->prev;
+        
+        ptr_to_return->next = ptrToFreeMemAfterSplit;
+        ptr_to_return->size = size;
     }
-
+        
     
-    ptr_to_return->size = size;
+    /* Removing the block - If pointer to the free block to be
+     * allocated is same as pointer to the head of free list, 
+     * the head pointer should be moved to point the next free
+     * block. Otherwise just remove the free block from the list
+     * and connect the adjacent ones.
+     */
+    if(ptr_to_return == hd_free_list)
+        hd_free_list =  ptrToFreeMemAfterSplit;        
+    else
+        ptr_to_return->prev->next = ptr_to_return->next;
+
     
     return (void *)ptr_to_return->buffer;
 
@@ -110,51 +132,51 @@ void* MyMalloc(int size)
 
 void MyFree(void *buffer)
 {    
-    malloc_overhead_t * ptr_to_mem_to_add = (malloc_overhead_t *) ((char *)buffer - SIZE_OVERHD);
+    malloc_overhead_t * ptr_to_free_mem = (malloc_overhead_t *) ((char *) buffer - SIZE_OVERHD);
     malloc_overhead_t * ptr_temp = hd_free_list;
     
-    if(!buffer)
+    if(!buffer || (arr_malloc > (char *) buffer) || (arr_malloc+MAX_MALLOC_SIZE < (char *) buffer))
         return;
     
     /* assign the memory to the free memory list and keep the head pointer
-       of the list as the least address (sorted based on address)*/
-    
-    if((ptr_to_int_t) ptr_to_mem_to_add < (ptr_to_int_t) hd_free_list)
+       of the list as the least address (sorted based on address) */   
+    if((ptr_to_int_t) ptr_to_free_mem < (ptr_to_int_t) hd_free_list)
     {
-        ptr_to_mem_to_add->next = hd_free_list;
-        ptr_to_mem_to_add->prev = NULL;
-        hd_free_list->prev = ptr_to_mem_to_add;
+        ptr_to_free_mem->next = hd_free_list;
+        ptr_to_free_mem->prev = NULL;
         
-        hd_free_list = ptr_to_mem_to_add;
+        if(hd_free_list->size!=0)
+            hd_free_list->prev = ptr_to_free_mem;
+        
+        hd_free_list = ptr_to_free_mem;
     }
     else
     {
-        while((ptr_to_int_t)ptr_temp->next < (ptr_to_int_t) ptr_to_mem_to_add)
+        printf("ptr : %p %p\n", ptr_temp, ptr_to_free_mem);
+        while((ptr_to_int_t)ptr_temp->next < (ptr_to_int_t) ptr_to_free_mem)
         {
             ptr_temp = ptr_temp->next;
-            if(!ptr_temp)
-                break;
         }
-        
-        ptr_to_mem_to_add->next = ptr_temp->next;
-        ptr_to_mem_to_add->prev = ptr_temp;
-        ptr_temp->next = ptr_to_mem_to_add;
+        printf("ptr : %p %p\n", ptr_temp, ptr_to_free_mem);
+        ptr_to_free_mem->next = ptr_temp->next;
+        ptr_to_free_mem->prev = ptr_temp;
+        ptr_temp->next = ptr_to_free_mem;
     }
     
     
-    // merging memories if adjacent (on the right)
-    if((ptr_to_int_t)(ptr_to_mem_to_add->buffer + ptr_to_mem_to_add->size) == (ptr_to_int_t)ptr_to_mem_to_add->next)
+    // merging memories if adjacent on the right
+    if((ptr_to_int_t)(ptr_to_free_mem->buffer + ptr_to_free_mem->size) == (ptr_to_int_t)ptr_to_free_mem->next)
     {
-        ptr_to_mem_to_add->size += ptr_to_mem_to_add->next->size+SIZE_OVERHD;
-        ptr_to_mem_to_add->next = ptr_to_mem_to_add->next->next;
+        ptr_to_free_mem->size += ptr_to_free_mem->next->size+SIZE_OVERHD;
+        ptr_to_free_mem->next = ptr_to_free_mem->next->next;
     }
-    // merging memories if adjacent (on the left)
-    if (ptr_to_mem_to_add->prev)
+    // merging memories if adjacent on the left
+    if (ptr_to_free_mem->prev)
     {
-        if((ptr_to_int_t)(ptr_to_mem_to_add->prev->buffer + ptr_to_mem_to_add->prev->size) == (ptr_to_int_t) ptr_to_mem_to_add)
+        if((ptr_to_int_t)(ptr_to_free_mem->prev->buffer + ptr_to_free_mem->prev->size) == (ptr_to_int_t) ptr_to_free_mem)
         {
-            ptr_to_mem_to_add->prev->size += ptr_to_mem_to_add->size;
-            ptr_to_mem_to_add->prev->next = ptr_to_mem_to_add->next;        
+            ptr_to_free_mem->prev->size += ptr_to_free_mem->size;
+            ptr_to_free_mem->prev->next = ptr_to_free_mem->next;        
         }
     }
 }
--- proj1_malloc/simpletest1.c
+++ proj1_malloc/simpletest1.c
@@ -7,6 +7,10 @@
 int main(int argc, char *argv[])
 {
 	char *array;
+    char * ar1;
+    char * ar2;
+    char * ar3;
+    char * ar4;
 
 	int i;
 
@@ -15,16 +19,47 @@ int main(int argc, char *argv[])
 	 */
 	InitMyMalloc();
 
-	array = MyMalloc(16352);
+	array = MyMalloc(16);
    	PrintMyMallocFreeList();
+    
+    //ar1 = MyMalloc(16);
+    //PrintMyMallocFreeList();
+    
+    ar1 = MyMalloc(32);
+    PrintMyMallocFreeList();
+    
+    ar2 = MyMalloc(256);
+    PrintMyMallocFreeList();
+    
+    ar3 = MyMalloc(128);
+    PrintMyMallocFreeList();
+    
+    ar4 = MyMalloc(256);
+    PrintMyMallocFreeList();
+    
+    MyFree(ar1);
+    PrintMyMallocFreeList();
+    
+    MyFree(ar3);
+    PrintMyMallocFreeList();
+    
+    //ar1 = MyMalloc(40);
+    //PrintMyMallocFreeList();
 
 	if(array == NULL)
 	{
-		fprintf(stderr,"call to MyMalloc() failed\n");
+		fprintf(stderr,"call to array MyMalloc() failed\n");
 		fflush(stderr);
 		exit(1);
 	}
 
+	if(ar1 == NULL)
+	{
+		fprintf(stderr,"call to ar MyMalloc() failed\n");
+		fflush(stderr);
+		exit(1);
+	}
+	
 	for(i=0; i < 9; i++)
 	{
 		array[i] = 'a' + i;
@@ -34,7 +69,11 @@ int main(int argc, char *argv[])
 	printf("here is my nifty new string: %s\n",array);
 
 	MyFree(array);
-    	PrintMyMallocFreeList();
+    PrintMyMallocFreeList();
+    
+    //MyFree(ar3);
+    //PrintMyMallocFreeList();
+
 
 
 	return(0);
