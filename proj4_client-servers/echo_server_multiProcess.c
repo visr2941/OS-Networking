ab524ff2a6f5b3ff4a476156b0eb271be5cda2c0
visr2941 <visr2941@colorado.edu>
2018-03-28 21:43:05 -0500

    added a short brief at top!

--- proj4_client-servers/echo_server_multiProcess.c
+++ proj4_client-servers/echo_server_multiProcess.c
@@ -1,3 +1,7 @@
+/* This is an echo server - it will print the data as received from the client. This server can listen to the multiple 
+** client at once using parent-child process. This can accept request from any IP address.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
