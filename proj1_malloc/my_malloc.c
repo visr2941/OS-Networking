b338e9ac50d1f2ec823807b3aeb217f4f427dfd2
visr2941 <visr2941@colorado.edu>
2021-03-04 17:35:10 -0800

    updated my malloc - clean implementation

--- proj1_malloc/my_malloc.c
+++ proj1_malloc/my_malloc.c
@@ -1,247 +1,233 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "my_malloc.h"
-#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "my_malloc.h"
 
-
-/*********************************************************************************
- ***************** MACRO Definitions *********************************************
- ********************************************************************************/
-
-#define SIZE_OVERHD      (sizeof(mallocOvrHd_t))
-#define ALIGN8(size)     if(size%8) size = size - (size%8 - 8)
-
-/*********************************************************************************
- ***************** Type Definitions **********************************************
- ********************************************************************************/
-
-// double link list structure for the allocation overhead
-typedef struct mallocOvrHd
-{
-    struct mallocOvrHd * next;
-    struct mallocOvrHd * prev;
+/************************************************************************************************************
+ *                                             Typedefs
+ * **********************************************************************************************************/
+typedef struct _ListNode {
+    struct _ListNode* left;
+    struct _ListNode* right;
     int size;
-    unsigned char * buffer;
+    void* buffer;
+} ListNode_t;
 
-} mallocOvrHd_t;
+/************************************************************************************************************
+ *                                               MACROS
+ * **********************************************************************************************************/
+#define GET_BUFFER_PTR_FROM_HEAD(x) ((void*)((char*)x+sizeof(ListNode_t)))
+#define GET_HEAD_PTR_FROM_BUFFER(x) ((ListNode_t*)((char*)x-sizeof(ListNode_t)))
+#define HEADER_SIZE (sizeof(ListNode_t))
 
-typedef long unsigned int int_t;
-typedef mallocOvrHd_t * ptrMallocOvrHd_t;
+/************************************************************************************************************
+ *                                      Static Functions Prototype
+ * **********************************************************************************************************/
+static int   _FixAlignment8Bytes(int sz);
+static void  _InsertFreeNode(ListNode_t* l_node, ListNode_t* r_node, ListNode_t* newNode);
+static void  _ProcessNodeFound(ListNode_t* tmpHead, int size);
+static void* _FirstFitAlgorithm(int size);
+static void  _CoalesceFreeNodes(ListNode_t* node);
 
-/*********************************************************************************
- ***************** GLOBAL Variables **********************************************
- ********************************************************************************/
+/************************************************************************************************************
+ *                                          Global Variable
+ * **********************************************************************************************************/
+// a global ptr to free list
+ListNode_t* head;
+unsigned char BigBuffer[MAX_MALLOC_SIZE];
 
-// defining a global variable to track the head pointer of the free memory list
-mallocOvrHd_t * hdrFreeList = NULL;
-// defining an array for malloc allocation
-char arrayMalloc[MAX_MALLOC_SIZE];
-
-
-/*********************************************************************************
- ***************** Function Definitions ******************************************
- ********************************************************************************/
-
-void InitMyMalloc( void )
+/************************************************************************************************************
+ *                                      Global Function Definitions
+ * **********************************************************************************************************/
+void InitMyMalloc()
 {
-    hdrFreeList = (mallocOvrHd_t *) arrayMalloc;
-
-    /* initializing the free memory list */
-    hdrFreeList->next = NULL;
-    hdrFreeList->prev = NULL;
-    hdrFreeList->size = MAX_MALLOC_SIZE - SIZE_OVERHD;
-    hdrFreeList->buffer = (unsigned char *) (arrayMalloc + SIZE_OVERHD);
-
+    head = (ListNode_t*)BigBuffer;
+    head->left = NULL;
+    head->right = NULL;
+    head->buffer = GET_BUFFER_PTR_FROM_HEAD(head);
+    head->size = MAX_MALLOC_SIZE - sizeof(ListNode_t);
 }
 
-/*************************************************************************
-*  Description:
-*            This function provides pointer to a valid memory of requested
-*            size based on the FIRST FIT method.
-*  Parameter:
-*            int size - size of memory required in bytes
-*  return:
-*            void * - a pointer of memory location as a void *
-**************************************************************************/
-void* MyMalloc(int size)
+void *MyMalloc(int size)
 {
-    mallocOvrHd_t * ptrAllocMemBlock          = hdrFreeList;
-    mallocOvrHd_t * ptrToFreeMemAfterSplit = NULL;
-
-    // Make the size divisible by 8 (greater or equal to size)
-    ALIGN8(size);
-
-    // finding the first fit
-    while(size > ptrAllocMemBlock->size)
-    {
-        ptrAllocMemBlock = ptrAllocMemBlock->next;
-        if(!ptrAllocMemBlock)
-            return NULL;
-    };
-
-    /* if we can't put overhead in remaining free space,
-     * we will allocate that memory to the user (wasted)
-     * otherwise split the free block into two memory
-     * blocks, first one to be allocated and next as free.
-     */
-    if(ptrAllocMemBlock->size - size <= SIZE_OVERHD)
-    {
-        size = ptrAllocMemBlock->size;
-        if(ptrAllocMemBlock->next)
-            ptrToFreeMemAfterSplit = ptrAllocMemBlock->next;
-	// if the memory to be allocated is the last block in the 
-	// Free List, set the head pointer to the end of the Array
-        else
-        {
-            ptrToFreeMemAfterSplit         = (ptrMallocOvrHd_t) (arrayMalloc+MAX_MALLOC_SIZE);
-            ptrToFreeMemAfterSplit->size   = 0;
-            ptrToFreeMemAfterSplit->buffer = NULL;
-            ptrToFreeMemAfterSplit->prev   = NULL;
-            ptrToFreeMemAfterSplit->next   = NULL;
-        }
-    }
-    // split the block selected
-    else
-    {
-        ptrToFreeMemAfterSplit         =  (ptrMallocOvrHd_t) ((char *) ptrAllocMemBlock + SIZE_OVERHD + size);
-        ptrToFreeMemAfterSplit->size   =  ptrAllocMemBlock->size - size - SIZE_OVERHD;
-        ptrToFreeMemAfterSplit->buffer =  (unsigned char *) ((char *) ptrToFreeMemAfterSplit + SIZE_OVERHD);
-        ptrToFreeMemAfterSplit->next   =  ptrAllocMemBlock->next;
-        ptrToFreeMemAfterSplit->prev   =  ptrAllocMemBlock->prev;
-
-        ptrAllocMemBlock->next = ptrToFreeMemAfterSplit;
-        ptrAllocMemBlock->size = size;
-    }
-
-
-    /* Removing the block - If pointer to the free block to be
-     * allocated is same as pointer to the head of free list,
-     * the head pointer should be moved to point the next free
-     * block. Otherwise just remove the free block from the list
-     * and connect the adjacent ones.
-     */
-    if(ptrAllocMemBlock == hdrFreeList)
-        hdrFreeList =  ptrToFreeMemAfterSplit;
-    else
-        ptrAllocMemBlock->prev->next = ptrAllocMemBlock->next;
-
-
-    return (void *)ptrAllocMemBlock->buffer;
-
+    size = _FixAlignment8Bytes(size);
+    return _FirstFitAlgorithm(size);
 }
 
-
-
-/*************************************************************************
-*  Description:
-*            This function frees the memeory provided by the user and adds 
-*            that up into the Free List of memory
-*  Parameter:
-*            void *buffer - pointer of the memory to be freed
-*  return:
-*            void - Nothing
-**************************************************************************/
 void MyFree(void *buffer)
 {
-    mallocOvrHd_t * ptrFreeMemBlock = (ptrMallocOvrHd_t) ((char *) buffer - SIZE_OVERHD);
-    mallocOvrHd_t * ptrTempForLoop  = hdrFreeList;
-
-    if(!buffer || (arrayMalloc > (char *) buffer) || (arrayMalloc + MAX_MALLOC_SIZE < (char *) buffer))
-        return;
-
-    /* add the memory to the free memory list and keep the head pointer
-       of the list as the least address (sorted based on address) */
-    if((int_t) ptrFreeMemBlock < (int_t) hdrFreeList)
+    if (buffer != NULL && ((long int)buffer & -248L))
     {
-        ptrFreeMemBlock->prev = NULL;
-	
-        if(hdrFreeList->size!=0)
+        ListNode_t* tmpHead = head;
+        ListNode_t* node = GET_HEAD_PTR_FROM_BUFFER(buffer);
+
+        // if all memory was allocated
+        if (head == NULL)
         {
-            hdrFreeList->prev = ptrFreeMemBlock;
-            ptrFreeMemBlock->next = hdrFreeList;
+            head = node;
+            return;
         }
-	// if the freed memory is the only block in the Free List,
-	// it's next pointer should be NULL.
+
+        if ((long int)tmpHead < (long int)node)
+        {
+            // traverse through the free list and insert 
+            // based on address
+            while (tmpHead->right != NULL)
+            {
+                if ((long int)tmpHead->right < (long int)node)
+                {
+                    tmpHead = tmpHead->right;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // insert the new free node
+            node->right = tmpHead->right;
+            tmpHead->right = node;
+            node->left = tmpHead;
+        }
         else
         {
-            ptrFreeMemBlock->next = NULL;
+            tmpHead->left = node;
+            node->right = tmpHead;
+            head = node;
         }
 
-        hdrFreeList = ptrFreeMemBlock;
-    }
-    // if freeing the same pointer more than once, it should return (do nothing)
-    else if ((int_t) ptrFreeMemBlock == (int_t) hdrFreeList)
-        return;
-    // the free memory block will not replace the head block of Free List, so 
-    // search the list for the reight spot to add it
-    else
-    {
-        while((int_t)ptrTempForLoop->next < (int_t) ptrFreeMemBlock)
-        {
-	    // breaking the loop because the free block fits at the
-	    // end of the Free List of memory
-            if(!ptrTempForLoop->next)
-                break;
-            ptrTempForLoop = ptrTempForLoop->next;
-        }
-
-        // if freeing the same pointer more than once, it should return (do nothing)
-        if((int_t)ptrTempForLoop->next == (int_t) ptrFreeMemBlock )
-            return;
-	
-	// adding the freed memory to the Free List of memory
-        ptrFreeMemBlock->next = ptrTempForLoop->next;
-        ptrFreeMemBlock->prev = ptrTempForLoop;
-        ptrTempForLoop->next = ptrFreeMemBlock;
-    }
-
-
-    // merging memories if adjacent on the right
-    if((int_t)(ptrFreeMemBlock->buffer + ptrFreeMemBlock->size) == (int_t)ptrFreeMemBlock->next)
-    {
-        ptrFreeMemBlock->size += ptrFreeMemBlock->next->size+SIZE_OVERHD;
-        ptrFreeMemBlock->next = ptrFreeMemBlock->next->next;
-    }
-    // merging memories if adjacent on the left
-    if (ptrFreeMemBlock->prev)
-    {
-        if((int_t)(ptrFreeMemBlock->prev->buffer + ptrFreeMemBlock->prev->size) == (int_t) ptrFreeMemBlock)
-        {
-            ptrFreeMemBlock->prev->size += ptrFreeMemBlock->size+SIZE_OVERHD;
-            ptrFreeMemBlock->prev->next = ptrFreeMemBlock->next;
-        }
+        // Coalesce if required
+        _CoalesceFreeNodes(node);
     }
 }
 
-
-/*************************************************************************
-*  Description:
-*            This function prints the list of free memory
-*  Parameter:
-*            void  - Nothing
-*  return:
-*            void  - Nothing
-**************************************************************************/
-void PrintMyMallocFreeList( void )
+void PrintMyMallocFreeList(void)
 {
-    mallocOvrHd_t * ptrTempForLoop = hdrFreeList;
-
-    if(hdrFreeList->size < 0)
+    ListNode_t* tmpHead = head;
+    while (tmpHead != NULL)
     {
-        fprintf(stderr,"No free heap memory - call to print free memory list failed\n");
-		fflush(stderr);
-		return;
+        printf("BlockPtr %p, BlockPtr->left %p, BlockPtr->right %p, BlockPtr->size %d\n", \
+                tmpHead, tmpHead->left, tmpHead->right, tmpHead->size);
+        
+        tmpHead = tmpHead->right;
     }
-    printf("\nPRINTING FREE MEMORY LIST...\n");
-    do
+}
+
+/************************************************************************************************************
+ *                                      Static Functions Definitions
+ * **********************************************************************************************************/
+static void* _FirstFitAlgorithm(int size)
+{
+    ListNode_t* tmpHead = head;
+
+    // On the first go, we will try to find a better match so that
+    // we can accomodate a header block for another free Node. If 
+    // we don't find any such node, we try fit in the closest
+    // size free node even if it doesn't have space for free node
+    // header
+    while (tmpHead->right != NULL && size > tmpHead->size + HEADER_SIZE)
     {
-        printf("block: %p\n", ptrTempForLoop);
-        printf("    size: %d\n", ptrTempForLoop->size);
-        printf("    next: %p\n", ptrTempForLoop->next);
-        printf("    prev: %p\n", ptrTempForLoop->prev);
-        printf("    buffer: %p\n", ptrTempForLoop->buffer);
+        tmpHead = tmpHead->right;
+    }
 
-        ptrTempForLoop = ptrTempForLoop->next;
+    // if node found on the first traverse
+    if (size <= (tmpHead->size + HEADER_SIZE))
+    {
+        // Free space found, update the free list and the 
+        // newly allocated node header
+        _ProcessNodeFound(tmpHead, size);
+    }
+    else
+    {
+        // 2nd traverse to find the node
+        tmpHead = head;
+        while(tmpHead != NULL)
+        {
+            while (tmpHead->right != NULL && size > tmpHead->size)
+            {
+                tmpHead = tmpHead->right;
+            }
 
-    } while (ptrTempForLoop);
+            if ((tmpHead->size - size) < HEADER_SIZE)
+            {
+                break;
+            }
+
+            tmpHead = tmpHead->right;
+        }
+
+        if (tmpHead != NULL)
+        {
+            // Free space found, update the free list and the 
+            // newly allocated node header
+            _ProcessNodeFound(tmpHead, size);
+        }
+    }
+
+    return (tmpHead != NULL ? tmpHead->buffer : NULL);
+}
+
+static int _FixAlignment8Bytes(int sz)
+{
+    return ((sz+7) >> 3) << 3;
+}
+
+static void _InsertFreeNode(ListNode_t* l_node, ListNode_t* r_node, ListNode_t* newNode)
+{
+    l_node->right = newNode;
+    newNode->right = r_node;
+}
+
+static void _ProcessNodeFound(ListNode_t* tmpHead, int size)
+{
+    int newSize = tmpHead->size - size - HEADER_SIZE;
+    tmpHead->size = size;
+    ListNode_t* newNode = (ListNode_t*)((char*)GET_BUFFER_PTR_FROM_HEAD(tmpHead) + size);
+    newNode->size = newSize;
+
+    if(tmpHead != head)
+    {
+        // update header of newly free node
+        _InsertFreeNode(tmpHead->left, tmpHead->right, newNode);
+    }
+    else
+    {
+        head = newNode;
+    }
+
+    // update allocated node header
+    tmpHead->left = NULL;
+    tmpHead->right = NULL;
+    tmpHead->buffer = GET_BUFFER_PTR_FROM_HEAD(tmpHead);
+}
+
+static void  _CoalesceFreeNodes(ListNode_t* node)
+{
+    if (node != NULL)
+    {
+        ListNode_t* l = node->left, *r = node->right;
+
+        // merge with left node
+        if (l != NULL)
+        {
+            if ((ListNode_t*)((char*)(l->buffer) + l->size) == l->right)
+            {
+                l->size += node->size;
+                l->right = node->right;   
+            }
+
+            node = l;
+        }
+
+        // merge with right node
+        if ((ListNode_t*)((char*)(node->buffer) + node->size) == node->right)
+        {
+            node->size += r->size;
+            node->right = r->right;  
+        }
+    }
+
+    return;
 }
